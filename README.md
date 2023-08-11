868d8122402c21dbd2fa0f038e9bfaa970556953
adarsh-jaiss <its.adarshjaiss@gmail.com>
2023-08-11 20:01:16 +0530

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# go-weather
-This is a weather forecast application built in Go
+# Introduction
+
+- This is a simple CLI weather application built using the weather API in Go
+
+# How to USE?
+
+- Clone the repo
+- `cd weather`
+- open the main.go file and in the URL section,add your desired location and then
+- run this command in the terminal `go run main.go`
+- You will see this output
+
+```
+Bhopal, India : 25°C, Mist
+20:00 - 25°C, 72, Patchy rain possible
+21:00 - 25°C, 0, Partly cloudy
+22:00 - 24°C, 0, Partly cloudy
+23:00 - 24°C, 67, Patchy rain possible
+```
+
+# How to contribute?
+
+### Features to Add
+
+1. Currently it only fetches weather report for a single location, we neeed to add a variable in a the URL so that we can pass multiple locations.
+2. Import the color module so that if shows red after reaching to a thresold value.
+
+- *Add these features and I'll merge the PR*
+- *If you are a frontend dev, you can create a frontend for this project and integrate it*
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	
+)
+
+const myAPI = "https://api.weatherapi.com/v1/forecast.json?key=6827c1cd51ee44e3902192605230604&q=Bhopal&days=1&aqi=no&alerts=no"
+
+func main() {
+	response, err := http.Get(myAPI)
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		panic("Weather API is not working")
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var weather Weather
+	err = json.Unmarshal(body, &weather)
+	if err != nil {
+		panic(err)
+	}
+
+	location, current, hours := weather.Location, weather.Current, weather.Forecast.Forecastday[0].Hour
+
+	fmt.Printf("%s, %s : %.0f°C, %s\n", location.Name, location.Country, current.TempC, current.Condition.Text)
+
+	for _, hour := range hours {
+		date := time.Unix(hour.TimeEpoch, 0)
+		myReport := fmt.Sprintf("%s - %.0f°C, %.0f, %s\n", date.Format("15:04"), hour.TempC, hour.Chanceofrain, hour.Condition.Text)
+
+		if date.Before(time.Now()){
+			continue
+		}
+		fmt.Print(myReport)
+	}
+
+	// You can add conditional formatting based on the chance of rain here if needed
+	// if hour.Chanceofrain < 40 {
+	// 	fmt.Print(myReport)
+	// }else{
+	// 	color.Red(myReport)
+	// }
+}
+
+type Weather struct {
+	Location struct {
+		Name    string `json:"name"`
+		Country string `json:"country"`
+	} `json:"location"`
+	Current struct {
+		TempC     float64 `json:"temp_c"`
+		Condition struct {
+			Text string `json:"text"`
+		} `json:"condition"`
+	} `json:"current"`
+	Forecast struct {
+		Forecastday []struct {
+			Hour []struct {
+				TimeEpoch    int64   `json:"time_epoch"`
+				TempC        float64 `json:"temp_c"`
+				Condition    struct {
+					Text string `json:"text"`
+				} `json:"condition"`
+				Chanceofrain float64 `json:"chance_of_rain"`
+			} `json:"hour"`
+		} `json:"forecastday"`
+	} `json:"forecast"`
+}
